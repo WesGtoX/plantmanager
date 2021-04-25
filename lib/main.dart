6fc01cbaec8bfe41806256f4164e0f3a34abe9fc
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2021-04-25 12:39:01 -0300

    feat: home widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'screens/HomeWidget.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'PlantManager',
+
+    // routes
+    initialRoute: '/home',
+    routes: {
+      '/home': (context) => Home(),
+    },
   ));
 }
---
+++ lib/screens/HomeWidget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+// Home
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    );
+  }
+}
