a94f56d46874d6fc6c3d0727365bdd16d0bbddcc
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-04-26 20:23:39 -0300

    atualizados estilos textuais

--- lib/core/app_text_styles.dart
+++ lib/core/app_text_styles.dart
@@ -5,32 +5,44 @@ import 'app_colors.dart';
 
 class AppTextStyles {
   static final TextStyle headingLarge = GoogleFonts.roboto(
-    color: AppColors.heading_color,
+    color: AppColors.headingColor,
     fontSize: 32,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle headingSmall = GoogleFonts.roboto(
-    color: AppColors.heading_color,
+    color: AppColors.headingColor,
     fontSize: 24,
     fontWeight: FontWeight.w600,
   );
 
   static final TextStyle textLarge = GoogleFonts.roboto(
-    color: AppColors.heading_color,
+    color: AppColors.headingColor,
     fontSize: 17,
     fontWeight: FontWeight.w400,
   );
 
   static final TextStyle textSmall = GoogleFonts.roboto(
-    color: AppColors.heading_color,
+    color: AppColors.headingColor,
     fontSize: 15,
     fontWeight: FontWeight.w400,
   );
-  
+
   static final TextStyle complement = GoogleFonts.roboto(
-    color: AppColors.heading_color,
+    color: AppColors.headingColor,
     fontSize: 13,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle textRetangle = GoogleFonts.roboto(
+    color: AppColors.retangleColor,
+    fontSize: 15,
+    fontWeight: FontWeight.w400,
+  );
+
+  static final TextStyle textLargeLight = GoogleFonts.roboto(
+    color: AppColors.headingLightColor,
+    fontSize: 17,
     fontWeight: FontWeight.w400,
   );
 }
