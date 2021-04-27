dc46b19115c3bdae13c0a0718eda9e2733360a92
Quemuel Alves Nassor <quemuelalp@hotmail.com>
2021-04-27 11:29:29 -0300

    implementada listView de itens

---
+++ lib/screens/Home/widgets/plants_list_view_widget.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantmanager/core/core.dart';
+
+class PlantsListViewWidget extends StatelessWidget {
+  final String imageUri;
+  final String label;
+  final DateTime alarm;
+  const PlantsListViewWidget({Key key, this.imageUri, this.label, this.alarm})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        width: 148,
+        height: 100,
+        padding: EdgeInsets.symmetric(horizontal: 30),
+        margin: EdgeInsets.only(bottom: 10),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: AppColors.shapeColor),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 21),
+                child: SvgPicture.network(
+                  imageUri,
+                )),
+            Text(
+              label,
+              style: AppTextStyles.textMedium,
+              textAlign: TextAlign.left,
+            ),
+            Text.rich(
+              TextSpan(
+                text: "Regar às\n",
+                style: AppTextStyles.smallText,
+                children: [
+                  TextSpan(
+                    text: ('${alarm.hour}:${alarm.minute}'),
+                    style: AppTextStyles.smallTextMedium,
+                  )
+                ],
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ));
+  }
+}
